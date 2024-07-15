a46859b4c76fe0227762d66fe545d4881b9a0408
human.soft <human.soft@hotmail.com>
2024-07-16 01:17:42 +0700

    (piw) run receive message queue on concurrent

--- main.go
+++ main.go
@@ -4,9 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"math/rand"
 	"os"
-	"strconv"
 	"sync"
 	"time"
 
@@ -21,71 +19,63 @@ var (
 
 const (
 	maxConcurrent = 5
-	totalTasks    = 10
+	maxMessage    = 10
+	ctxTimeout    = 10 // second
 )
 
 func main() {
 	loadenv()
-	// runConcurrentTasks()
-	runQueue()
-}
 
-func loadenv() {
-	// Load environment variables from .env file
-	if err := godotenv.Load(); err != nil {
-		log.Fatalf("Error loading .env file: %s", err)
-	}
-
-	connectionString = os.Getenv("CONNECTION_STRING")
-	queueName = os.Getenv("QUEUE_NAME")
-}
-
-func runQueue() {
 	// สร้าง Service Bus Client
 	client, err := azservicebus.NewClientFromConnectionString(connectionString, nil)
 	if err != nil {
 		log.Fatalf("Failed to create client: %s", err)
 	}
 
+	for {
+		fmt.Println("run...")
+		receiveMessageQueue(client)
+	}
+}
+
+func receiveMessageQueue(client *azservicebus.Client) {
 	// สร้าง Context สำหรับการทำงาน
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Minute)
+	ctx, cancel := context.WithTimeout(context.Background(), ctxTimeout*time.Second)
 	defer cancel()
 
 	// รับ Session Receiver สำหรับ queue
 	sessionReceiver, err := client.AcceptNextSessionForQueue(ctx, queueName, nil)
 	if err != nil {
-		log.Fatalf("Failed to accept next session: %s", err)
+		// log.Fatalf("Failed to accept next session: %s", err)
+		log.Printf("Failed to accept next session: %s", err)
+		return
 	}
 	fmt.Printf("<===== Accept Session ID: %s =====>\n", sessionReceiver.SessionID())
 	defer sessionReceiver.Close(ctx)
 
 	// Loop เพื่อรับและประมวลผลข้อความ
 	for {
-		msgs, err := sessionReceiver.ReceiveMessages(ctx, 1, nil)
+		msgs, err := sessionReceiver.ReceiveMessages(ctx, maxMessage, nil)
+		fmt.Println("have messages.")
 		if err != nil {
-			log.Fatalf("Failed to receive messages: %s", err)
-		}
-
-		for _, msg := range msgs {
-			fmt.Printf("Received message: %s\n", string(msg.Body))
-
-			// Complete ข้อความ
-			err = sessionReceiver.CompleteMessage(ctx, msg, nil)
-			if err != nil {
-				log.Fatalf("Failed to complete message: %s", err)
+			if ctx.Err() == context.DeadlineExceeded {
+				log.Printf("Context deadline exceeded, reinitializing context and retrying...")
+				break
+			} else {
+				log.Fatalf("Failed to receive messages: %s", err)
 			}
 		}
+
+		processMessages(ctx, sessionReceiver, msgs)
 	}
 }
 
-func runConcurrentTasks() {
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-
+func processMessages(ctx context.Context, sessionReceiver *azservicebus.SessionReceiver, msgs []*azservicebus.ReceivedMessage) {
 	// สร้าง channel สำหรับส่งงาน
-	tasks := make(chan string, totalTasks)
+	tasks := make(chan *azservicebus.ReceivedMessage, len(msgs))
 
 	// สร้าง semaphore เพื่อจำกัดจำนวน concurrent requests
-	sem := make(chan struct{}, maxConcurrent)
+	sem := make(chan int, maxConcurrent)
 
 	// สร้าง WaitGroup เพื่อรอให้ goroutines ทำงานเสร็จ
 	var wg sync.WaitGroup
@@ -97,9 +87,11 @@ func runConcurrentTasks() {
 			defer wg.Done()
 			for task := range tasks {
 				// รอให้มี slot ว่างใน semaphore
-				sem <- struct{}{}
+				sem <- 1
+
 				// ส่ง request ไปยัง endpoint
-				sendRequest(task, r)
+				postRequest(ctx, sessionReceiver, task)
+
 				// ปล่อย slot ใน semaphore
 				<-sem
 			}
@@ -107,23 +99,30 @@ func runConcurrentTasks() {
 	}
 
 	// ส่งงานไปยัง channel
-	for i := 1; i <= totalTasks; i++ {
-		tasks <- "Message " + strconv.Itoa(i)
+	for _, msg := range msgs {
+		tasks <- msg
 	}
 	close(tasks)
 
-	// รอให้ goroutines ทำงานเสร็จ
 	wg.Wait()
 }
 
-func sendRequest(payload string, r *rand.Rand) {
-	sleep(payload, r)
+func postRequest(ctx context.Context, sessionReceiver *azservicebus.SessionReceiver, task *azservicebus.ReceivedMessage) {
+	fmt.Printf("Received message: %s\n", string(task.Body))
+
+	// Complete ข้อความ
+	err := sessionReceiver.CompleteMessage(ctx, task, nil)
+	if err != nil {
+		log.Fatalf("Failed to complete message: %s", err)
+	}
 }
 
-func sleep(payload string, r *rand.Rand) {
-	sleepDuration := r.Intn(1001) + 1000
+func loadenv() {
+	// Load environment variables from .env file
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading .env file: %s", err)
+	}
 
-	fmt.Printf("Send %s: %d ms\n", payload, sleepDuration)
-	time.Sleep(time.Duration(sleepDuration) * time.Millisecond)
-	fmt.Printf("Done %s!\n", payload)
+	connectionString = os.Getenv("CONNECTION_STRING")
+	queueName = os.Getenv("QUEUE_NAME")
 }
