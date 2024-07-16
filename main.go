3b69f1bbfd7c520d6e5d1565cf42b2534d70db2d
Ponlapat <human.soft@hotmail.com>
2024-07-16 12:12:10 +0700

    (piw) finish post request

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
-	"math/rand"
+	"net/http"
 	"os"
 	"sync"
 	"time"
@@ -119,15 +122,38 @@ func processMessages(ctx context.Context, sessionReceiver *azservicebus.SessionR
 }
 
 func postRequest(ctx context.Context, sessionReceiver *azservicebus.SessionReceiver, task *azservicebus.ReceivedMessage) {
-	r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	sleepDuration := r.Intn(1001) + 100
+	// เริ่มการวัดเวลา
+	start := time.Now()
 
-	fmt.Printf("Received message: %s %dms\n", string(task.Body), sleepDuration)
-	sleep(sleepDuration)
-	fmt.Printf("Done: %s\n", string(task.Body))
+	// Printf task.Body จาก []byte เป็น string
+	fmt.Printf("Received message: %s\n", string(task.Body))
+
+	// URL ที่ต้องการส่ง request ไป
+	url := defineUrl(task)
+
+	// สร้าง HTTP POST request
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(task.Body))
+	if err != nil {
+		log.Fatalf("Failed to create POST request: %s", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	// ส่ง request โดยใช้ http.Client
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalf("Failed to send POST request: %s", err)
+	}
+	defer resp.Body.Close()
+
+	// อ่าน response body
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Failed to read response body: %s", err)
+	}
 
 	// Complete ข้อความ
-	err := sessionReceiver.CompleteMessage(ctx, task, nil)
+	err = sessionReceiver.CompleteMessage(ctx, task, nil)
 	if err != nil {
 		if ctx.Err() == context.DeadlineExceeded {
 			log.Printf("Failed to complete message: %s, reinitializing context and retrying...", err)
@@ -135,13 +161,46 @@ func postRequest(ctx context.Context, sessionReceiver *azservicebus.SessionRecei
 			log.Fatalf("Failed to complete message: %s", err)
 		}
 	}
+
+	// สิ้นสุดการวัดเวลา
+	elapsed := time.Since(start)
+
+	// แสดงผล response status, body และ elapsed time
+	fmt.Printf("\nResponse status: %s\n", resp.Status)
+	fmt.Printf("Response body: %s\n", body)
+	fmt.Printf("Elapsed time: %.2f seconds\n", elapsed.Seconds())
 }
 
-func sleep(sleepDuration int) {
-	// r := rand.New(rand.NewSource(time.Now().UnixNano()))
-	// sleepDuration := r.Intn(1001) + 100
-	time.Sleep(time.Duration(sleepDuration) * time.Millisecond)
+func defineUrl(task *azservicebus.ReceivedMessage) string {
+	var url string
+
+	// แปลง task.Body จาก []byte เป็น map[string]interface{}
+	var data map[string]interface{}
+	err := json.Unmarshal(task.Body, &data)
+	if err != nil {
+		log.Fatalf("Failed to unmarshal message body: %s", err)
+	}
+
+	// ตรวจสอบว่ามี key "url" ในข้อมูลหรือไม่
+	if val, ok := data["url"].(string); ok {
+		// fmt.Printf("Found 'url' key in message body: %s\n", val)
+		url = val
+	} else {
+		// fmt.Println("No 'url' key found in message body")
+		url = "http://localhost/api-server/api-test.php"
+	}
+
+	// Force define url for test
+	// url = "http://localhost/api-server/api-test.php"
+
+	return url
 }
+
+/* func sleep() {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	sleepDuration := r.Intn(1001) + 100
+	time.Sleep(time.Duration(sleepDuration) * time.Millisecond)
+} */
 
 func loadenv() {
 	// Load environment variables from .env file
